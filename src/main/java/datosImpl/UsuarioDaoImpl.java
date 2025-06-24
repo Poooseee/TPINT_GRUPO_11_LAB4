e2ed52b7c31cbe579ec034e446477624e96bb5cb
AlejoGomezDev <alejongomez@gmail.com>
2025-06-23 21:20:51 -0300

    dao usuario

---
+++ src/main/java/datosImpl/UsuarioDaoImpl.java
@@ -0,0 +1,48 @@
+package datosImpl;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import datos.UsuarioDao;
+import entidades.Usuario;
+
+public class UsuarioDaoImpl implements UsuarioDao {
+	
+	private Conexion cn;
+	
+	@Override
+	public Usuario login(Usuario usuario) throws SQLException {
+		Usuario u = null;
+		
+		try {
+			cn = new Conexion();
+			cn.Open();
+			
+			String query = "SELECT idUsuario_Usr as id, tipo_Usr as tipo FROM USUARIOS WHERE nick_Usr = ? and contraseña_Usr = ?";
+			
+			PreparedStatement ps = cn.prepare(query);
+			ps.setString(1, usuario.getNickUsuario());
+			ps.setString(2, usuario.getContraseñaUsuario());
+			
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				u = new Usuario();
+				u.setIdUsuario(rs.getInt("id"));
+				u.setTipoUsuario(rs.getString("tipo"));
+			}
+			
+			rs.close();
+			ps.close();
+			cn.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		return u;
+	}
+
+}
