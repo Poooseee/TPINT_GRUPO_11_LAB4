73dccb67cf8181aa1eff6a99f194da3b819d25bf
liocoma <lionelcorradini@gmail.com>
2025-07-08 15:35:45 -0300

    ..

--- src/main/java/datosImpl/UsuarioDaoImpl.java
+++ src/main/java/datosImpl/UsuarioDaoImpl.java
@@ -13,32 +13,32 @@ public class UsuarioDaoImpl implements UsuarioDao {
 	
 	@Override
 	public Usuario login(Usuario usuario) throws SQLException {
-	    Usuario u = null;
-	    
-	    try {
-	        cn = new Conexion();
-	        cn.Open();
-	        
-	        String query = "SELECT idUsuario_Usr, tipo_Usr FROM usuarios WHERE nick_Usr = ? AND contraseña_Usr = ?";
-	        
-	        PreparedStatement ps = cn.prepare(query);
-	        ps.setString(1, usuario.getNickUsuario());
-	        ps.setString(2, usuario.getContraseñaUsuario());
-	        
-	        ResultSet rs = ps.executeQuery();
-	        
-	        if(rs.next()) {
-	            u = new Usuario();
-	            u.setIdUsuario(rs.getInt("idUsuario_Usr")); 
-	            u.setTipoUsuario(rs.getString("tipo_Usr")); 
-
-	        } else {
-	            System.out.println("\nNo se encontró usuario con esas credenciales");
-	        }
-	        
-	        rs.close();
-	        ps.close();
-	    }
+		Usuario u = null;
+		
+		try {
+			cn = new Conexion();
+			cn.Open();
+			
+			String query = "SELECT idUsuario_Usr as id, tipo_Usr as tipo FROM USUARIOS WHERE nick_Usr = ? and contraseña_Usr = ?";
+			
+			PreparedStatement ps = cn.prepare(query);
+			ps.setString(1, usuario.getNickUsuario());
+			ps.setString(2, usuario.getContraseñaUsuario());
+			
+			ResultSet rs = ps.executeQuery();
+			System.out.println();
+			if(rs.next()) {
+				u = new Usuario();
+				u.setIdUsuario(rs.getInt("id"));
+				u.setTipoUsuario(rs.getString("tipo"));
+				System.out.println("usuario en dao: "+u.getIdUsuario());
+				System.out.println("usuario en dao: "+u.getTipoUsuario());
+			}
+			
+			rs.close();
+			ps.close();
+			cn.close();
+		}
 	    catch(Exception e) {
 	        System.out.println("\nERROR en UsuarioDaoImpl:");
 	        e.printStackTrace();
