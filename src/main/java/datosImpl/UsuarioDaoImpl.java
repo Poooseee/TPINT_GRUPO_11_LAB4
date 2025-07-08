a5572aa2976aeb3eb7ee54bbc690814acf2bbf7e
liocoma <lionelcorradini@gmail.com>
2025-07-08 15:27:12 -0300

    ..

--- src/main/java/datosImpl/UsuarioDaoImpl.java
+++ src/main/java/datosImpl/UsuarioDaoImpl.java
@@ -15,18 +15,11 @@ public class UsuarioDaoImpl implements UsuarioDao {
 	public Usuario login(Usuario usuario) throws SQLException {
 	    Usuario u = null;
 	    
-	    System.out.println("\n--- INICIO DE DEPURACIÓN UsuarioDaoImpl ---");
-	    System.out.println("Credenciales recibidas:");
-	    System.out.println("Usuario: " + usuario.getNickUsuario());
-	    System.out.println("Contraseña: " + usuario.getContraseñaUsuario());
-	    
 	    try {
 	        cn = new Conexion();
 	        cn.Open();
 	        
-	        // CONSULTA 100% CORRECTA CON TUS NOMBRES DE COLUMNA
 	        String query = "SELECT idUsuario_Usr, tipo_Usr FROM usuarios WHERE nick_Usr = ? AND contraseña_Usr = ?";
-	        System.out.println("\nConsulta SQL exacta: " + query);
 	        
 	        PreparedStatement ps = cn.prepare(query);
 	        ps.setString(1, usuario.getNickUsuario());
@@ -36,13 +29,9 @@ public class UsuarioDaoImpl implements UsuarioDao {
 	        
 	        if(rs.next()) {
 	            u = new Usuario();
-	            // USANDO LOS NOMBRES EXACTOS DE TUS COLUMNAS
-	            u.setIdUsuario(rs.getInt("idUsuario_Usr"));  // Cambiado a idUsuario_Usr
-	            u.setTipoUsuario(rs.getString("tipo_Usr"));   // Cambiado a tipo_Usr
-	            
-	            System.out.println("\nUsuario encontrado en BD:");
-	            System.out.println("ID: " + u.getIdUsuario());
-	            System.out.println("Tipo: " + u.getTipoUsuario());
+	            u.setIdUsuario(rs.getInt("idUsuario_Usr")); 
+	            u.setTipoUsuario(rs.getString("tipo_Usr")); 
+
 	        } else {
 	            System.out.println("\nNo se encontró usuario con esas credenciales");
 	        }
@@ -60,7 +49,6 @@ public class UsuarioDaoImpl implements UsuarioDao {
 	        }
 	    }
 	    
-	    System.out.println("--- FIN DE DEPURACIÓN UsuarioDaoImpl ---\n");
 	    return u;
 	}
 }
